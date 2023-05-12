1f0bb261a6b61192875dbeef725b4a74de307bd1
Mady Seta <ralaimadyseta@gmail.com>
2023-05-12 19:42:54 +0200

    Ajout du mécanisme de nmobre de tour

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { PublishModalComponent } from '../publish-modal/publish-modal.component';
+import { Globals } from 'src/data/sharedData';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  styleUrls: ['./main-page.component.css'],
+  providers : [Globals]
 })
 export class MainPageComponent {
+  
   constructor(public dialog: MatDialog){}
+  global = Globals;
 
   openDialog() {
     const dialogRef = this.dialog.open(PublishModalComponent, {
